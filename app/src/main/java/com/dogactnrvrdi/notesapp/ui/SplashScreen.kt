e3e7a4d344f443b366605e0739022380508e0d48
Dogac Tanriverdi <d.tnrvrdi@proton.me>
2023-10-22 09:17:52 +0300

    Add animation to splash screen

--- app/src/main/java/com/dogactnrvrdi/notesapp/ui/SplashScreen.kt
+++ app/src/main/java/com/dogactnrvrdi/notesapp/ui/SplashScreen.kt
@@ -1,35 +1,33 @@
 package com.dogactnrvrdi.notesapp.ui
 
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
 import com.dogactnrvrdi.notesapp.R
+import com.dogactnrvrdi.notesapp.databinding.ActivitySplashScreenBinding
 
 class SplashScreen : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashScreenBinding
 
     private var runnable = Runnable {}
     private var handler = Handler(Looper.getMainLooper())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val animFadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in)
+        binding.logoOpen.startAnimation(animFadeIn)
 
         supportActionBar?.hide()
 
-        if (Build.VERSION.SDK_INT < 32) {
-            runnable = Runnable {
-                val intent = Intent(
-                    this@SplashScreen,
-                    MainActivity::class.java
-                )
-                startActivity(intent)
-                finish()
-            }
-            handler.postDelayed(runnable, 1000)
-        } else {
+        runnable = Runnable {
             val intent = Intent(
                 this@SplashScreen,
                 MainActivity::class.java
@@ -37,5 +35,6 @@ class SplashScreen : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+        handler.postDelayed(runnable, 1500)
     }
 }
