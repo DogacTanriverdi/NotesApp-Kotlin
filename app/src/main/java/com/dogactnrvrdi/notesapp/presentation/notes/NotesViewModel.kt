1d055f5682477ff3f3574f575ad9fa6b1a26907f
Dogac TANRIVERDI <d.tnrvrdi@proton.me>
2025-01-20 22:23:13 +0300

    Update notes view model according to notes contract

--- app/src/main/java/com/dogactnrvrdi/notesapp/presentation/notes/NotesViewModel.kt
+++ app/src/main/java/com/dogactnrvrdi/notesapp/presentation/notes/NotesViewModel.kt
@@ -1,17 +1,20 @@
 package com.dogactnrvrdi.notesapp.presentation.notes
 
-import androidx.compose.runtime.State
-import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.dogactnrvrdi.notesapp.common.NoteOrder
 import com.dogactnrvrdi.notesapp.common.OrderType
 import com.dogactnrvrdi.notesapp.data.model.Note
 import com.dogactnrvrdi.notesapp.domain.usecase.NoteUseCases
+import com.dogactnrvrdi.notesapp.presentation.notes.NotesContract.UiAction
+import com.dogactnrvrdi.notesapp.presentation.notes.NotesContract.UiEffect
+import com.dogactnrvrdi.notesapp.presentation.notes.NotesContract.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -20,71 +23,96 @@ class NotesViewModel @Inject constructor(
     private val noteUseCases: NoteUseCases
 ) : ViewModel() {
 
-    private val _state = mutableStateOf(NotesState())
-    val state: State<NotesState> = _state
+    private val _uiState = MutableStateFlow(UiState())
+    val uiState = _uiState.asStateFlow()
 
-    private var recentlyDeletedNote: Note? = null
+    private val _uiEffect by lazy { Channel<UiEffect>() }
+    val uiEffect by lazy { _uiEffect.receiveAsFlow() }
 
-    private var getNotesJob: Job? = null
+    private fun updateUiState(block: UiState.() -> UiState) {
+        _uiState.update(block)
+    }
 
-    private var searchNoteJob: Job? = null
+    private suspend fun emitUiEffect(uiEffect: UiEffect) {
+        _uiEffect.send(uiEffect)
+    }
 
-    init {
-        getNotes(NoteOrder.Date(OrderType.Descending))
+    fun onAction(uiAction: UiAction) {
+        when (uiAction) {
+
+            is UiAction.Order -> order(uiAction.noteOrder)
+
+            is UiAction.ToggleOrderSection -> toggleOrderSection()
+
+            is UiAction.FabClick -> {
+                viewModelScope.launch {
+                    emitUiEffect(
+                        UiEffect.NavigateToAddEditNoteScreen(
+                            noteId = uiAction.noteId,
+                            noteColor = uiAction.noteColor
+                        )
+                    )
+                }
+            }
+
+            is UiAction.GetNotes -> getNotes(uiAction.noteOrder)
+
+            is UiAction.DeleteNote -> deleteNote(uiAction.note)
+
+            UiAction.RestoreNote -> restoreNote()
+
+            is UiAction.SearchNote -> {
+                searchNote(query = uiAction.query, noteOrder = uiAction.noteOrder)
+            }
+
+            is UiAction.ShowSnackbar -> {
+                viewModelScope.launch {
+                    emitUiEffect(UiEffect.ShowSnackbar(uiAction.message, uiAction.actionLabel))
+                }
+            }
+        }
     }
 
     private fun getNotes(noteOrder: NoteOrder) {
-        getNotesJob?.cancel()
-        getNotesJob = noteUseCases.getNotes(noteOrder)
-            .onEach { notes ->
-                _state.value = state.value.copy(
-                    notes = notes,
-                    noteOrder = noteOrder
-                )
-            }
-            .launchIn(viewModelScope)
-    }
-
-    fun searchNote(query: String, noteOrder: NoteOrder = NoteOrder.Date(OrderType.Descending)) {
-        searchNoteJob?.cancel()
-        searchNoteJob = noteUseCases.searchNote("%$query%", noteOrder)
-            .onEach { notes ->
-                _state.value = state.value.copy(
-                    notes = notes,
-                    noteOrder = noteOrder
-                )
-            }.launchIn(viewModelScope)
-    }
-
-    fun searchSection() {
-        _state.value = state.value.copy(
-            isSearchSectionVisible = !state.value.isSearchSectionVisible
-        )
-    }
-
-    fun toggleOrderSection() {
-        _state.value = state.value.copy(
-            isOrderSectionVisible = !state.value.isOrderSectionVisible
-        )
-    }
-
-    fun restoreNote() {
         viewModelScope.launch {
-            noteUseCases.addNote(recentlyDeletedNote ?: return@launch)
-            recentlyDeletedNote = null
+            noteUseCases.getNotes(noteOrder).collect { notes ->
+                updateUiState { copy(notes = notes, noteOrder = noteOrder) }
+            }
         }
     }
 
-    fun deleteNote(note: Note) {
+    private fun searchNote(
+        query: String,
+        noteOrder: NoteOrder = NoteOrder.Date(OrderType.Descending)
+    ) {
+        viewModelScope.launch {
+            noteUseCases.searchNote("%$query%", noteOrder).collect { notes ->
+                updateUiState { copy(notes = notes, noteOrder = noteOrder) }
+            }
+        }
+    }
+
+    private fun toggleOrderSection() {
+        updateUiState { copy(isOrderSectionVisible = !isOrderSectionVisible) }
+    }
+
+    private fun restoreNote() {
+        viewModelScope.launch {
+            noteUseCases.addNote(uiState.value.recentlyDeletedNote ?: return@launch)
+            updateUiState { copy(recentlyDeletedNote = null) }
+        }
+    }
+
+    private fun deleteNote(note: Note) {
         viewModelScope.launch {
             noteUseCases.deleteNote(note)
-            recentlyDeletedNote = note
+            updateUiState { copy(recentlyDeletedNote = note) }
         }
     }
 
-    fun order(noteOrder: NoteOrder) {
-        if (state.value.noteOrder::class == noteOrder::class &&
-            state.value.noteOrder.orderType == noteOrder.orderType
+    private fun order(noteOrder: NoteOrder) {
+        if (uiState.value.noteOrder::class == noteOrder::class &&
+            uiState.value.noteOrder.orderType == noteOrder.orderType
         ) {
             return
         }
