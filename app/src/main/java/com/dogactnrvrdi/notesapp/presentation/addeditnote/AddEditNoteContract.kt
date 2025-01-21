8048dd527e1efb62edc2163c2f4d54593a027598
Dogac TANRIVERDI <d.tnrvrdi@proton.me>
2025-01-21 11:32:17 +0300

    Add add edit note contract

---
+++ app/src/main/java/com/dogactnrvrdi/notesapp/presentation/addeditnote/AddEditNoteContract.kt
@@ -0,0 +1,20 @@
+package com.dogactnrvrdi.notesapp.presentation.addeditnote
+
+import com.dogactnrvrdi.notesapp.data.model.Note
+
+object AddEditNoteContract {
+
+    data class UiState(
+        val currentNoteId: Int? = null,
+        val note: Note? = null
+    )
+
+    sealed interface UiEffect {
+        data object NavigateToNotesScreen: UiEffect
+    }
+
+    sealed interface UiAction {
+        data class SaveNote(val note: Note): UiAction
+        data class GetNote(val noteId: Int): UiAction
+    }
+}
