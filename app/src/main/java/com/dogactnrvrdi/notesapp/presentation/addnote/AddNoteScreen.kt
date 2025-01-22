62b1d07c3e855b5b4ac480784c6da88f0403f0db
Dogac TANRIVERDI <d.tnrvrdi@proton.me>
2025-01-22 18:04:21 +0300

    Add add note screen and components

---
+++ app/src/main/java/com/dogactnrvrdi/notesapp/presentation/addnote/AddNoteScreen.kt
@@ -0,0 +1,197 @@
+package com.dogactnrvrdi.notesapp.presentation.addnote
+
+import androidx.compose.animation.Animatable
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Save
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.dogactnrvrdi.notesapp.R
+import com.dogactnrvrdi.notesapp.data.model.Note
+import com.dogactnrvrdi.notesapp.presentation.addnote.AddNoteContract.UiAction
+import com.dogactnrvrdi.notesapp.presentation.addnote.AddNoteContract.UiEffect
+import com.dogactnrvrdi.notesapp.presentation.addnote.components.AddNoteScreenTopBar
+import com.dogactnrvrdi.notesapp.presentation.addnote.components.ColorSection
+import com.dogactnrvrdi.notesapp.presentation.addnote.components.DescriptionTextField
+import com.dogactnrvrdi.notesapp.presentation.addnote.components.TitleTextField
+import com.dogactnrvrdi.notesapp.presentation.components.CustomFab
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
+
+@Composable
+fun AddNoteScreen(
+    navController: NavController,
+    uiEffect: Flow<UiEffect>,
+    onAction: (UiAction) -> Unit
+) {
+    val snackbarHostState = remember { SnackbarHostState() }
+
+    val coroutineScope = rememberCoroutineScope()
+
+
+    LaunchedEffect(uiEffect) {
+        uiEffect.collect { effect ->
+            when (effect) {
+
+                is UiEffect.ShowSnackbar -> {
+                    coroutineScope.launch {
+                        snackbarHostState.showSnackbar(
+                            message = effect.message,
+                            actionLabel = effect.actionLabel
+                        )
+                    }
+                }
+
+                is UiEffect.NavigateBack -> navController.navigateUp()
+            }
+        }
+    }
+
+    AddNoteContent(
+        onAction = onAction
+    )
+}
+
+@Composable
+fun AddNoteContent(
+    onAction: (UiAction) -> Unit
+) {
+    val context = LocalContext.current
+
+    val coroutineScope = rememberCoroutineScope()
+
+    var isColorSectionVisible by remember { mutableStateOf(false) }
+
+    var color by remember { mutableIntStateOf(Note.getRandomColor()) }
+
+    val noteBackgroundAnimatable = remember { Animatable(Color(color)) }
+
+    var title by remember { mutableStateOf("") }
+    var description by remember { mutableStateOf("") }
+
+    Scaffold(
+        topBar = {
+            AddNoteScreenTopBar(
+                onColorClick = { isColorSectionVisible = !isColorSectionVisible },
+                onBackClick = { onAction(UiAction.BackClick) }
+            )
+        },
+        floatingActionButton = {
+            CustomFab(
+                icon = Icons.Default.Save,
+                contentDescription = stringResource(R.string.save_note_button),
+            ) {
+                if (title.isBlank() || description.isBlank()) {
+                    onAction(
+                        UiAction.ShowSnackbar(
+                            message = context.getString(R.string.title_or_description_cannot_be_empty),
+                            actionLabel = null
+                        )
+                    )
+                    return@CustomFab
+                }
+
+                onAction(
+                    UiAction.SaveNote(
+                        note = Note(
+                            title = title,
+                            description = description,
+                            color = color
+                        )
+                    )
+                )
+            }
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.background)
+                .padding(innerPadding),
+        ) {
+
+            AnimatedVisibility(
+                visible = isColorSectionVisible,
+                enter = fadeIn() + slideInVertically(),
+                exit = fadeOut() + slideOutVertically()
+            ) {
+                ColorSection(
+                    modifier = Modifier
+                        .background(MaterialTheme.colorScheme.surface)
+                        .fillMaxWidth()
+                        .padding(vertical = 16.dp),
+                    scope = coroutineScope,
+                    noteColor = color,
+                    noteBackgroundAnimatable = noteBackgroundAnimatable
+                ) { selectedColor ->
+                    color = selectedColor
+                }
+            }
+
+            Box(
+                modifier = Modifier
+                    .height(5.dp)
+                    .fillMaxWidth()
+                    .background(noteBackgroundAnimatable.value)
+            )
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            TitleTextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 10.dp),
+                value = title,
+                onValueChange = { title = it },
+                hint = stringResource(R.string.title)
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            DescriptionTextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 10.dp),
+                value = description,
+                onValueChange = { description = it },
+                hint = stringResource(R.string.description)
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun AddNoteContentPreview() {
+    AddNoteContent(
+        onAction = {}
+    )
+}
---
+++ app/src/main/java/com/dogactnrvrdi/notesapp/presentation/addnote/components/DescriptionTextField.kt
@@ -0,0 +1,63 @@
+package com.dogactnrvrdi.notesapp.presentation.addnote.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun DescriptionTextField(
+    modifier: Modifier = Modifier,
+    value: String,
+    onValueChange: (String) -> Unit,
+    hint: String,
+) {
+    Box(modifier = modifier) {
+
+        if (value.isEmpty()) {
+            Text(
+                modifier = Modifier
+                    .padding(start = 4.dp),
+                text = hint,
+                color = Color.Gray,
+                textAlign = TextAlign.Center,
+                fontSize = MaterialTheme.typography.titleLarge.fontSize,
+            )
+        }
+
+        BasicTextField(
+            modifier = Modifier
+                .fillMaxWidth(),
+            value = value,
+            onValueChange = { newValue ->
+                onValueChange(capitalizeFirstLetter(newValue))
+            },
+            textStyle = TextStyle(
+                fontSize = MaterialTheme.typography.titleLarge.fontSize,
+                color = MaterialTheme.colorScheme.onBackground
+            ),
+            keyboardOptions = KeyboardOptions.Default.copy(
+                capitalization = KeyboardCapitalization.Words
+            ),
+            minLines = 50
+        )
+    }
+}
+
+fun capitalizeFirstLetter(input: String): String {
+    return if (input.isNotEmpty()) {
+        input.replaceFirstChar { it.uppercaseChar() }
+    } else {
+        input
+    }
+}
