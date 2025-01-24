78e6d6835c714b48629ad2399187b0803c1f2604
Dogac TANRIVERDI <d.tnrvrdi@proton.me>
2025-01-24 14:13:44 +0300

    Add note detail to screen file

--- app/src/main/java/com/dogactnrvrdi/notesapp/presentation/navigation/Screen.kt
+++ app/src/main/java/com/dogactnrvrdi/notesapp/presentation/navigation/Screen.kt
@@ -11,5 +11,8 @@ sealed interface Screen {
     data object AddNoteScreen : Screen
 
     @Serializable
+    data class NoteDetailScreen(val noteId: Int) : Screen
+
+    @Serializable
     data class AddEditNoteScreen(val noteId: Int = -1, val noteColor: Int = -1) : Screen
 }
