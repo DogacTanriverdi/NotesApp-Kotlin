5295e4508c30e079826e5d618b5a17372003c132
Dogac TANRIVERDI <d.tnrvrdi@proton.me>
2025-01-24 18:08:26 +0300

    Add update note

--- app/src/main/java/com/dogactnrvrdi/notesapp/data/source/local/NoteDao.kt
+++ app/src/main/java/com/dogactnrvrdi/notesapp/data/source/local/NoteDao.kt
@@ -3,8 +3,8 @@ package com.dogactnrvrdi.notesapp.data.source.local
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
-import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import com.dogactnrvrdi.notesapp.data.model.Note
 import kotlinx.coroutines.flow.Flow
 
@@ -17,11 +17,14 @@ interface NoteDao {
     @Query("SELECT * FROM notes WHERE id = :id")
     suspend fun getNoteById(id: Int): Note?
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    @Insert
     suspend fun insertNote(note: Note)
 
     @Delete
     suspend fun deleteNote(note: Note)
+
+    @Update
+    suspend fun updateNote(note: Note)
 
     @Query("SELECT * FROM notes WHERE title LIKE :query OR description LIKE :query")
     fun searchNote(query: String?): Flow<List<Note>>
