1404725a8d117a081e87a89fec35364ff52d2683
Dogac TANRIVERDI <d.tnrvrdi@proton.me>
2025-01-24 18:11:19 +0300

    Add edit note screen top bar

---
+++ app/src/main/java/com/dogactnrvrdi/notesapp/presentation/editnote/components/EditNoteScreenTopBar.kt
@@ -0,0 +1,67 @@
+package com.dogactnrvrdi.notesapp.presentation.editnote.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ColorLens
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.dogactnrvrdi.notesapp.R
+
+@Composable
+fun EditNoteScreenTopBar(
+    modifier: Modifier = Modifier,
+    onColorClick: () -> Unit,
+    onBackClick: () -> Unit,
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colorScheme.surface)
+            .height(70.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+
+        Row(
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            IconButton(onClick = onBackClick) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = stringResource(R.string.back_button),
+                    tint = MaterialTheme.colorScheme.onBackground
+                )
+            }
+
+            Text(
+                text = stringResource(R.string.edit_note),
+                color = MaterialTheme.colorScheme.onBackground,
+                style = MaterialTheme.typography.headlineMedium,
+                modifier = Modifier.padding(start = 15.dp)
+            )
+        }
+
+        IconButton(onClick = onColorClick) {
+            Icon(
+                imageVector = Icons.Default.ColorLens,
+                contentDescription = stringResource(R.string.select_color_button),
+                tint = MaterialTheme.colorScheme.onBackground
+            )
+        }
+    }
+}
