3f5ce698f7d6accc6d901352ea825fe099127188
Dogac TANRIVERDI <d.tnrvrdi@proton.me>
2025-01-24 10:54:47 +0300

    Add imePadding

--- app/src/main/java/com/dogactnrvrdi/notesapp/presentation/addnote/AddNoteScreen.kt
+++ app/src/main/java/com/dogactnrvrdi/notesapp/presentation/addnote/AddNoteScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Save
@@ -140,7 +141,8 @@ fun AddNoteContent(
             modifier = Modifier
                 .fillMaxSize()
                 .background(MaterialTheme.colorScheme.background)
-                .padding(innerPadding),
+                .padding(innerPadding)
+                .imePadding(),
         ) {
 
             AnimatedVisibility(
