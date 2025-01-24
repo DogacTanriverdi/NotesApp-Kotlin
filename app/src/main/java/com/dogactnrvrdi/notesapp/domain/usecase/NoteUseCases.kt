59f07ed70d22a311609cde931a2216f7bec96129
Dogac TANRIVERDI <d.tnrvrdi@proton.me>
2025-01-24 18:09:09 +0300

    Add update note use case to note use cases

--- app/src/main/java/com/dogactnrvrdi/notesapp/domain/usecase/NoteUseCases.kt
+++ app/src/main/java/com/dogactnrvrdi/notesapp/domain/usecase/NoteUseCases.kt
@@ -4,6 +4,7 @@ data class NoteUseCases(
     val getNotes: GetNotesUseCase,
     val deleteNote: DeleteNoteUseCase,
     val addNote: AddNoteUseCase,
+    val updateNote: UpdateNoteUseCase,
     val getNote: GetNoteUseCase,
     val searchNote: SearchNoteUseCase
 )
